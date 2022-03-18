d95975c4933229c74a3f698a994ac02da2dd5ded
Ian Ellison-Taylor <ianet@microsoft.com>
2022-03-18 15:53:38 -0700

    ...

--- src/LibStereoKit.jl
+++ src/LibStereoKit.jl
@@ -1,18 +1,14 @@
 module LibStereoKit
 
 using CEnum
-using Pkg.Artifacts
 
 # 
 # Automatically generated file
 #
+using Pkg.Artifacts
 
 StereoKitC = joinpath(artifact"StereoKitC", "StereoKitC\\runtimes\\win-x64\\native\\StereoKitC.dll")
 
-# function __init__()
-#     # TODO - make a proper 'artifact'
-#     chmod(StereoKitC, filemode(StereoKitC) | 0o755) # dll needs to executable
-# end
 
 const uint64_t = Culonglong
 
