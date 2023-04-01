cd18ba100ba12897bbf471f461364192c032f30c
ianet <ianet@users.noreply.github.com>
2023-04-01 13:36:21 -0700

    some helpers

--- src/LibStereoKit.jl
+++ src/LibStereoKit.jl
@@ -3285,4 +3285,59 @@ const SK_VERSION_PRERELEASE = 0
 
 const SK_VERSION_ID = ((uint64_t(SK_VERSION_MAJOR) << 48 | uint64_t(SK_VERSION_MINOR) << 32) | uint64_t(SK_VERSION_PATCH) << 16) | uint64_t(SK_VERSION_PRERELEASE)
 
+# Helpers
+
+Base.:(*)(a::vec3, b::vec3)::vec3 = vec3(a.x*b.x, a.y*b.y, a.z*b.z)
+Base.:(+)(a::vec3, b::vec3)::vec3 = vec3(a.x+b.x, a.y+b.y, a.z+b.z)
+
+function sk_renderloop(render::Function)::Void
+    render_c = @cfunction($render, Void, ()) # Not supported on all cpu architectures
+    if isinteractive()
+        while SK.sk_step(render_c) > 0; sleep(0.01) end
+    else
+        while SK.sk_step(render_c) > 0; end
+    end
+end
+
+function sk_init(;
+    app_name::String = "",
+    assets_folder::String = "",
+    display_preference::SK.display_mode_ = SK.display_mode_mixedreality,
+    blend_preference::SK.display_blend_ = SK.display_blend_any_transparent,
+    no_flatscreen_fallback::Bool = false,
+    depth_mode::SK.depth_mode_ = SK.depth_mode_balanced,
+    log_filter::SK.log_ = SK.log_diagnostic,
+    overlay_app::Bool = false,
+    overlay_priority::Int = 0, 
+    flatscreen_pos_x::Int = 0,
+    flatscreen_pos_y::Int = 0, 
+    flatscreen_width::Int = 0, 
+    flatscreen_height::Int = 0, 
+    disable_flatscreen_mr_sim::Bool = false,
+    disable_unfocused_sleep::Bool = true)
+
+    GC.@preserve app_name assets_folder begin 
+        settings = SK.sk_settings_t(
+            pointer(app_name),
+            pointer(assets_folder),
+            display_preference,
+            blend_preference,
+            no_flatscreen_fallback,
+            depth_mode,
+            log_filter,
+            overlay_app,
+            overlay_priority,
+            flatscreen_pos_x,
+            flatscreen_pos_y,
+            flatscreen_width,
+            flatscreen_height,
+            disable_flatscreen_mr_sim,
+            disable_unfocused_sleep,
+            C_NULL, 
+            C_NULL
+        )
+        SK.sk_init(settings)
+    end
+end
+
 end # module
